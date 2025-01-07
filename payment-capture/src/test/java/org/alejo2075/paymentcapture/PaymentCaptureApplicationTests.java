4d8b96bae9872ff73cc6ab56bd4c3a804ff94fd2
Alejo2075 <alejsant75@gmail.com>
2025-01-07 16:33:36 -0600

    add Payment Capture Module

---
+++ payment-capture/src/test/java/org/alejo2075/paymentcapture/PaymentCaptureApplicationTests.java
@@ -0,0 +1,13 @@
+package org.alejo2075.paymentcapture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentCaptureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
