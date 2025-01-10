0f5890cdebbc00b4f95f1fd70b0201126b7a4edc
Alejo2075 <alejsant75@gmail.com>
2025-01-10 17:41:54 -0600

    upgrade infrastrcture architecture

--- service-one/src/main/java/org/alejo2075/paymentauhorization/PaymentAuhorizationApplication.java
+++ service-one/src/main/java/org/alejo2075/paymentauhorization/PaymentAuhorizationApplication.java
@@ -2,8 +2,10 @@ package org.alejo2075.paymentauhorization;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class PaymentAuhorizationApplication {
 
     public static void main(String[] args) {
--- service-two/src/main/java/org/alejo2075/paymentdispute/PaymentDisputeApplication.java
+++ service-two/src/main/java/org/alejo2075/paymentdispute/PaymentDisputeApplication.java
@@ -2,8 +2,10 @@ package org.alejo2075.paymentdispute;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class PaymentDisputeApplication {
 
     public static void main(String[] args) {
