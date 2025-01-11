4f38c515f7cb9ef5a26128050d29ee0cc3ec0eb7
Alejo2075 <alejsant75@gmail.com>
2025-01-11 14:46:51 -0600

    add keycloak and docker

---
+++ api-gateway/src/main/java/org/alejo2075/apigateway/config/SecurityConfig.java
@@ -0,0 +1,17 @@
+package org.alejo2075.apigateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/public/**").permitAll()
+                        .anyRequest().authenticated()
+                );
+        return http.build();
+    }
+}
